956f5aff1822a96bff5e7a61ec97132cb9db0b5f
Jesús Chavez <chavezmele7030@gmail.com>
2026-02-17 13:29:07 -0600

    add translated stat labels for Pokemon details

---
+++ src/constants/PokemonStatLabels.ts
@@ -0,0 +1,8 @@
+export const STAT_LABELS: Record<string, string> = {
+  hp: 'HP',
+  attack: 'Ataque',
+  defense: 'Defensa',
+  'special-attack': 'Sp. Atq',
+  'special-defense': 'Sp. Def',
+  speed: 'Velocidad',
+};
