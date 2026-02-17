a785b63b2cd5b80cb8865f4f06b80924d019d1d4
Jesús Chavez <chavezmele7030@gmail.com>
2026-02-17 02:42:16 -0600

    fix: update IndexPage layout and component reference to use Pokemons

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,8 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <PokemonTester />
+  <q-page class="row items-center justify-center" style="min-height: 100vh">
+    <div class="full-width q-pa-sm" style="max-width: 1100px; box-sizing: border-box">
+      <Pokemons />
+    </div>
   </q-page>
 </template>
 
@@ -10,7 +12,7 @@ import { defineAsyncComponent, defineComponent } from 'vue';
 export default defineComponent({
   name: 'HomePage',
   components: {
-    PokemonTester: defineAsyncComponent(() => import('src/components/PokemonTester.vue')),
+    Pokemons: defineAsyncComponent(() => import('src/components/Pokemons.vue')),
   },
 });
 </script>
